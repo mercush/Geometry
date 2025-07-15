f5cf356ac7faf0125baa85bd60b48fdd147443bc
mercush <barba@mit.edu>
2025-07-14 20:22:53 -0400

    fix tactic

--- Geometry/Tactic.lean
+++ Geometry/Tactic.lean
@@ -5,9 +5,9 @@ open Lean Meta Elab Tactic
 macro "algebraic_euclid" : tactic =>
   `(tactic|
     -- Try to extract from any hypothesis that matches the pattern
-    (repeat first
+    (repeat (any_goals first
     | simp_all only [euclid_simp]
     | rw [Segment.length_sq_iff]
-    | constructor)
+    | constructor))
     <;>
     grind (splits := 0) -cutsat -linarith -mbtc)
